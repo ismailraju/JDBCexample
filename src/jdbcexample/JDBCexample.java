cd047bd57f2bb6baa90d195bbb7a4a8d7e133217
ismailraju <ismailhossainraju74@gmail.com>
2017-12-06 10:02:06 +0600

    DriverManager not work

--- src/jdbcexample/JDBCexample.java
+++ src/jdbcexample/JDBCexample.java
@@ -5,6 +5,8 @@
  */
 package jdbcexample;
 
+import java.sql.*;
+
 /**
  *
  * @author ismailu
@@ -15,7 +17,40 @@ public class JDBCexample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+
+        try {
+            System.out.println("try");
+            // 1. Get a connection to database
+            myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/US_LOCATIONS", "root", "PS");
+            System.out.println("myConn");
+
+            // 2. Create a statement
+            myStmt = myConn.createStatement();
+
+            // 3. Execute SQL query
+            myRs = myStmt.executeQuery("SELECT * FROM US_LOCATIONS.US_CITIES;");
+
+            // 4. Process the result set
+            while (myRs.next()) {
+                System.out.println(myRs.getString("CITY"));
+            }
+        } catch (Exception exc) {
+        } finally {
+            if (myRs != null) {
+                //myRs.close();
+            }
+
+            if (myStmt != null) {
+                // myStmt.close();
+            }
+
+            if (myConn != null) {
+                //myConn.close();
+            }
+        }
     }
 
 }
